b50863d3fe07a4ff22f84107649c522582ea1ec6
KAMADUSHKA <kamadushkachathuranga@gmail.com>
2023-05-06 23:42:02 +0530

    create link for login paget to registra page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
             path='/Dboard'
             element={<Dboard/>}
             />
-          
+           
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pagers/Login.js
+++ frontend/src/pagers/Login.js
@@ -50,6 +50,10 @@ export default function Login() {
                       </div>
 
                 </div>
+                        <div id='loginLink'>
+                        if you are not registered 
+                        <a href='./'>  click this</a>
+            </div>
 
       </fieldset>
         
